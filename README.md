c65d6abe7ac8537eb591fe912728e29657d49da1
luanalvesdaniel <luan-ad-daniel@hotmail.com>
2020-10-30 14:09:29 -0300

    ambiente e arrow functions

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # cypress
+
+- node.js
+- vscode
+
+npm init
+npm install cypress
+
+abrindo cypress
+cria-se um script no package.json
+"open": "cypress open"
+comando: npm run open
---
+++ cypress/integration/examples/arrow.spec.js
@@ -0,0 +1,29 @@
+
+it('nada agora', function(){})
+
+//Função padrão
+//function soma(a, b){
+//    return a + b
+//}
+
+//Função oculta
+//const soma = function (a, b){
+//    return a + b
+//}
+
+//Arrow Function
+//const soma = (a, b) => {
+//    return a + b
+//}
+
+//Arrow function mais simples
+const soma = (a, b) => a + b
+console.log(soma(1,2))
+
+it('a function test...', function(){
+    console.log('Function', this)
+})
+
+it('an arrow test...', () => {
+    console.log('Arrow', this)
+})
