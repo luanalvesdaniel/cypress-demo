e86cb12b67bb6b93b65c931568d61efb6cc0a476
luanalvesdaniel <luan-ad-daniel@hotmail.com>
2020-10-30 14:44:37 -0300

    describe, it, skip, only

---
+++ cypress/integration/examples/describe.spec.js
@@ -0,0 +1,26 @@
+/// <references types="cypress" />
+
+describe.only('Should group tests...', () => {
+
+    describe('Should group more tests...', () => {
+
+        it.skip('A internal test 1...', () => {
+
+
+
+        })
+
+    })
+
+    describe('Should group more tests...', () => {
+
+        it('A internal test 2...', () => {
+
+
+
+        })
+
+    })
+
+})
+
