68e78a37c66bb27d3a3a67fdb650688a6493cc4c
luanalvesdaniel <luan-ad-daniel@hotmail.com>
2020-10-30 19:05:52 -0300

    campos de texto

--- cypress/integration/examples/elements.spec.js
+++ cypress/integration/examples/elements.spec.js
@@ -35,4 +35,28 @@ describe('Work with basic elements', () => {
         cy.get('#resultado')
             .should('have.text', 'Voltou!')
     })
+
+    it('TextFields', () => {
+        cy.get('#formNome')
+            .type('Luan')
+            .should('have.value', 'Luan')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .type('abcdef')
+            .should('have.value', 'abcdef')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('blabla')
+            .should('have.value', 'blabla')   
+
+        cy.get('#formSobrenome')
+            .type('Alves Danielll{backspace}{backspace}')
+            .should('have.value', 'Alves Daniel')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', {delay: 200})
+            .should('have.value', 'acerto')
+
+    })
 })
