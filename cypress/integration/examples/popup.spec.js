59798ecc6eeead4d8df403ecd61645c55608f33f
luanalvesdaniel <luan-ad-daniel@hotmail.com>
2020-10-31 16:48:57 -0300

    popup via links

--- cypress/integration/examples/popup.spec.js
+++ cypress/integration/examples/popup.spec.js
@@ -2,30 +2,62 @@
 
 describe('Work with Popups', () => {
 
-    it('Deve testar popup diretamente', () => {
+    describe('Without Links...', () => {
 
-        cy.visit('https://wcaquino.me/cypress/frame.html')
+        it('Deve testar popup diretamente', () => {
 
-        cy.get('#otherButton')
-           .click()
+            cy.visit('https://wcaquino.me/cypress/frame.html')
 
-        cy.on('window:alert', msg => {
-            expect(msg).to.be.equal('Click OK!')
-        })       
-    }) 
-
-    it('Deve verificar se popup foi invocado', () => {
-
-        cy.visit('https://wcaquino.me/cypress/componentes.html')
-
-        cy.window().then(win => {
-            cy.stub(win, 'open').as('winOpen')
-        })
-        cy.get('#buttonPopUp')
+            cy.get('#otherButton')
             .click()
 
-        cy.get('@winOpen')
-            .should('be.called')
-       
+            cy.on('window:alert', msg => {
+                expect(msg).to.be.equal('Click OK!')
+            })       
+        }) 
+
+        it('Deve verificar se popup foi invocado', () => {
+
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+
+            cy.window().then(win => {
+                cy.stub(win, 'open').as('winOpen')
+            })
+            cy.get('#buttonPopUp')
+                .click()
+
+            cy.get('@winOpen')
+                .should('be.called')
+        
+        }) 
     }) 
+    describe('With Links...', () => {
+
+        beforeEach(() => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+        })
+
+        it('Check popup url', () => {
+            cy.contains('Popup2')
+                .should('have.prop', 'href')
+                .and('equal', 'https://wcaquino.me/cypress/frame.html')
+        })
+
+        it('Should acess popup dinamically', () => {
+            cy.contains('Popup2').then($a => {
+                const href = $a.prop('href')
+                cy.visit(href)
+                cy.get('#tfield')
+                    .type('Funciona')
+            })                
+        })
+
+        it('Should force link on same page', () => {
+            cy.contains('Popup2')
+                .invoke('removeAttr', 'target')
+                .click()
+                cy.get('#tfield')
+                    .type('Funciona')
+            })                
+        })    
 })
