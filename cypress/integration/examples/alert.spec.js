0cefd545902e67f5eca15752ba295527a3422c3a
luanalvesdaniel <luan-ad-daniel@hotmail.com>
2020-10-31 15:39:10 -0300

    alert confirm e deny

--- cypress/integration/examples/alert.spec.js
+++ cypress/integration/examples/alert.spec.js
@@ -34,4 +34,36 @@ describe('Work with alerts', () => {
         })
             
     })
+
+    it('Confirm', () => {
+        
+        cy.get('#confirm')
+            .click()
+
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Confirmado')
+        })
+
+    })
+
+    it('Deny', () => {
+        
+        cy.get('#confirm')
+            .click()
+
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+
+        cy.on('window:alert', msg => {
+            console.log(msg)
+            expect(msg).to.be.equal('Negado')
+        })
+
+    })
 })
