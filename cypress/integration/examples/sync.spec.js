579922e2802a21cc3caf08254b53edf8f55fc924
luanalvesdaniel <luan-ad-daniel@hotmail.com>
2020-10-31 11:03:55 -0300

    retentativas/retrys

--- cypress/integration/examples/sync.spec.js
+++ cypress/integration/examples/sync.spec.js
@@ -28,4 +28,24 @@ describe('Esperas...', () => {
             .type('Funciona')
     })
 
+    it.only('Deve fazer retrys', () => {
+        
+        cy.get('#novoCampo')
+            .should('not.exist')
+
+        cy.get('#buttonDelay')
+            .click()
+
+        cy.get('#novoCampo')
+            .should('not.exist')
+
+        cy.get('#novoCampo')
+            //.should('not.exist') //Não podemos usar pois retorna o mesmo elemento, com isso dá erro
+            .should('exist')
+            .type('Funciona')
+            
+
+    })
+
+
 })
