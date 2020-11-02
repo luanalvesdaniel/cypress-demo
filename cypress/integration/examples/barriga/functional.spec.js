c01090195a2bc2262d0655e99acfd2d253c7eacd
luanalvesdaniel <luan-ad-daniel@hotmail.com>
2020-11-02 16:41:33 -0300

    aplicacao real (funcional): inserir movimentacao

--- cypress/integration/examples/barriga/functional.spec.js
+++ cypress/integration/examples/barriga/functional.spec.js
@@ -39,4 +39,18 @@ describe('Deve testar a nivel funcional', () => {
         cy.get(loc.MESSAGE).should('contain', 'code 400')
 
     })
+
+    it('Deve criar uma movimentacao', () => {
+
+        cy.get(loc.MENU.MOVIMENTACAO).click()
+        cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
+        cy.get(loc.MOVIMENTACAO.VALOR).type('123')
+        cy.get(loc.MOVIMENTACAO.INTERESSADO).type('123')
+        cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
+        cy.get(loc.MESSAGE).should('contain', 'sucesso')
+
+        cy.get(loc.EXTRATO.LINHAS).should('have.length', 7)
+        cy.xpath(loc.EXTRATO.XP_BUSCA_ELEMENTO).should('exist')
+        
+    })
 })
