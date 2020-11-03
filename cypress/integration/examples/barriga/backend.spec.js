6cd0c3d95f265ea73250aed4c3e7227af9b8c250
luanalvesdaniel <luan-ad-daniel@hotmail.com>
2020-11-03 09:11:30 -0300

    aplicacao real (backend): inserindo conta

--- cypress/integration/examples/barriga/backend.spec.js
+++ cypress/integration/examples/barriga/backend.spec.js
@@ -11,7 +11,8 @@ describe('Deve testar a nivel funcional', () => {
         //cy.resetApp()
     })
 
-    it.only('Deve fazer login', () => {
+    it.only('Deve criar a conta', () => {
+        
         cy.request({
             method: 'POST',
             url: 'https://barrigarest.wcaquino.me/signin',
@@ -21,9 +22,22 @@ describe('Deve testar a nivel funcional', () => {
                 senha: "luan"
             }
         }).its('body.token').should('not.be.empty')
-    })
-
-    it('Deve criar a conta', () => {
+        .then(token => {
+            cy.request({
+                url: 'https://barrigarest.wcaquino.me/contas',
+                method: 'POST',
+                headers: { Authorization: `JWT ${token}`},
+                body: {
+                    nome: "Conta via rest3"
+                }
+            }).as('response')
+        })
+        
+        cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome', 'Conta via rest3')
+        })
 
     })
 
@@ -49,3 +63,4 @@ describe('Deve testar a nivel funcional', () => {
     })
 
 })
+
