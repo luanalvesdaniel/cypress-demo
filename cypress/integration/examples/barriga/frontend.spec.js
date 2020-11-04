ce0a905e257d65901ccf22298c03175b64b6cf51
luanalvesdaniel <luan-ad-daniel@hotmail.com>
2020-11-03 23:10:43 -0300

    aplicacao real (frontendmockado): inserindo e conferindo movimentacao

--- cypress/integration/examples/barriga/frontend.spec.js
+++ cypress/integration/examples/barriga/frontend.spec.js
@@ -71,7 +71,7 @@ describe('Deve testar a nivel frontend com mock', () => {
 
     })
 
-    it.only('Não deve inserir conta com mesmo nome', () => {
+    it('Não deve inserir conta com mesmo nome', () => {
 
         cy.route({
             method: 'POST',
@@ -88,13 +88,33 @@ describe('Deve testar a nivel frontend com mock', () => {
 
     })
 
-    it('Deve criar uma movimentacao', () => {
+    it.only('Deve criar uma movimentacao', () => {
+
+        cy.route({
+            method: 'POST',
+            url: '/transacoes',
+            response:
+                {"id":288215,"descricao":"fsdfds","envolvido":"sdfsdf","observacao":null,"tipo":"REC","data_transacao":"2020-11-03T03:00:00.000Z","data_pagamento":"2020-11-03T03:00:00.000Z","valor":"2.00","status":true,"conta_id":317574,"usuario_id":12145,"transferencia_id":null,"parcelamento_id":null}
+        })
+
+        cy.route({
+            method: 'GET',
+            url: '/extrato/**',
+            response: [
+                {"conta":"Conta para movimentacoes","id":288209,"descricao":"Movimentacao para exclusao","envolvido":"AAA","observacao":null,"tipo":"DESP","data_transacao":"2020-11-03T03:00:00.000Z","data_pagamento":"2020-11-03T03:00:00.000Z","valor":"-1500.00","status":true,"conta_id":317576,"usuario_id":12145,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta com movimentacao","id":288210,"descricao":"Movimentacao de conta","envolvido":"BBB","observacao":null,"tipo":"DESP","data_transacao":"2020-11-03T03:00:00.000Z","data_pagamento":"2020-11-03T03:00:00.000Z","valor":"-1500.00","status":true,"conta_id":317577,"usuario_id":12145,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta para saldo","id":288211,"descricao":"Movimentacao 1, calculo saldo","envolvido":"CCC","observacao":null,"tipo":"REC","data_transacao":"2020-11-03T03:00:00.000Z","data_pagamento":"2020-11-03T03:00:00.000Z","valor":"3500.00","status":false,"conta_id":317578,"usuario_id":12145,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta para saldo","id":288212,"descricao":"Movimentacao 2, calculo saldo","envolvido":"DDD","observacao":null,"tipo":"DESP","data_transacao":"2020-11-03T03:00:00.000Z","data_pagamento":"2020-11-03T03:00:00.000Z","valor":"-1000.00","status":true,"conta_id":317578,"usuario_id":12145,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta para saldo","id":288213,"descricao":"Movimentacao 3, calculo saldo","envolvido":"EEE","observacao":null,"tipo":"REC","data_transacao":"2020-11-03T03:00:00.000Z","data_pagamento":"2020-11-03T03:00:00.000Z","valor":"1534.00","status":true,"conta_id":317578,"usuario_id":12145,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta para extrato","id":288214,"descricao":"Movimentacao para extrato","envolvido":"FFF","observacao":null,"tipo":"DESP","data_transacao":"2020-11-03T03:00:00.000Z","data_pagamento":"2020-11-03T03:00:00.000Z","valor":"-220.00","status":true,"conta_id":317579,"usuario_id":12145,"transferencia_id":null,"parcelamento_id":null},
+                {"conta":"Conta para alterar","id":288215,"descricao":"Desc","envolvido":"sdfsdf","observacao":null,"tipo":"REC","data_transacao":"2020-11-03T03:00:00.000Z","data_pagamento":"2020-11-03T03:00:00.000Z","valor":"123.00","status":true,"conta_id":317574,"usuario_id":12145,"transferencia_id":null,"parcelamento_id":null}]
+        })
 
         cy.get(loc.MENU.MOVIMENTACAO).click()
         cy.get(loc.MOVIMENTACAO.DESCRICAO).type('Desc')
         cy.get(loc.MOVIMENTACAO.VALOR).type('123')
         cy.get(loc.MOVIMENTACAO.INTERESSADO).type('Inter')
-        cy.get(loc.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
+        cy.get(loc.MOVIMENTACAO.CONTA).select('Banco')
         cy.get(loc.MOVIMENTACAO.STATUS).click()
         cy.get(loc.MOVIMENTACAO.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'sucesso')
